ed21b473bfe03c2c0bc58897ac4b0f42eac43833
Daniele Pala <pala.daniele@gmail.com>
2015-12-17 17:24:34 +0100

    Various bugfixes and improvements.

--- js/cimTreeController.js
+++ js/cimTreeController.js
@@ -1,101 +1,127 @@
-let cimTreeController = {
-    
-    setModel: function(model) {
-	this.model = model;
-    },
+"use strict";
 
-    render: function(diagramName) {
-	// clear all
-	d3.select("#app-spreadsheet").select(".tree").remove();
+function cimTreeController() {
+    return {
 	
-	let diagram = this.model.getObjects("cim:Diagram")
-            .filter(el => el.getAttributes()
-		    .filter(el => el.nodeName === "cim:IdentifiedObject.name")[0].textContent===decodeURI(diagramName));
-	let allConnectivityNodes = this.model.getObjects("cim:ConnectivityNode");
-	console.log("extracted connectivity nodes");
-	let allACLines = this.model.getObjects("cim:ACLineSegment");
-	console.log("extracted acLines");
-	let allDiagramObjects = this.model.getObjects("cim:DiagramObject");
-	console.log("extracted diagramObjects");
-	if (diagram.length > 0) {
-	    allDiagramObjects = allDiagramObjects.filter(el => el.getLinks().filter(el => el.localName === "DiagramObject.Diagram")[0].attributes[0].value === "#" + diagram[0].attributes[0].value);
+	setModel: function(model) {
+	    this.model = model;
+	},
+
+	render: function(diagramName) {
+	    // clear all
+	    d3.select("#app-tree").select(".tree").remove();
+	    
+	    let diagram = this.model.getObjects("cim:Diagram")
+		.filter(el => el.getAttributes()
+			.filter(el => el.nodeName === "cim:IdentifiedObject.name")[0].textContent===decodeURI(diagramName));
+	    let allConnectivityNodes = this.model.getObjects("cim:ConnectivityNode");
+	    console.log("extracted connectivity nodes");
+	    let allACLines = this.model.getObjects("cim:ACLineSegment");
+	    console.log("extracted acLines");
+	    let allDiagramObjects = this.model.getObjects("cim:DiagramObject");
+	    console.log("extracted diagramObjects");
+	    if (diagram.length > 0) {
+		allDiagramObjects = allDiagramObjects.filter(el => el.getLinks().filter(el => el.localName === "DiagramObject.Diagram")[0].attributes[0].value === "#" + diagram[0].attributes[0].value);
+	    }
+	    let ioEdges = this.model.getGraph(allDiagramObjects, "DiagramObject.IdentifiedObject");
+	    if (diagram.length > 0) {
+		let graphicObjects = ioEdges.map(el => el.source);
+		allACLines = allACLines.filter(acl => graphicObjects.indexOf(acl) > -1);
+		allConnectivityNodes = allConnectivityNodes.filter(cn => typeof(cn.x) != "undefined");
+	    }
+	    let nodes = allACLines;
+	    
+	    // navtree
+	    let form = d3.select("#app-tree")
+		.append("div")
+		.attr("class", "tree")
+		.append("form")
+		.attr("class", "form-inline");
+	    form.append("div")
+		.attr("class", "form-group")
+		.append("input")
+		.attr("type", "search")
+		.attr("class", "form-control")
+		.attr("id", "cim-search-key")
+		.attr("placeholder", "Search...");
+	    form.append("button")
+		.attr("type", "submit")
+		.attr("class", "btn btn-default")
+		.html("Search").on("click", function() {
+		    let searchKey = document.getElementById("cim-search-key").value;
+		    $(".CIMNetwork>li>ul>li>a:contains(" + searchKey + ")").parent().show();
+		    $(".CIMNetwork>li>ul>li>a:not(:contains(" + searchKey + "))").parent().hide();
+		    console.log($(".CIMNetwork>li>ul>li>a:contains(" + searchKey + ")").size());
+		    $(".CIMNetwork>li>ul").each(function() {
+			console.log(this);
+		    });
+		});
+
+	    let cimNetwork = d3.select("div.tree")
+		.append("ul")
+		.attr("class", "CIMNetwork list-group");
+	    this.createElements(cimNetwork, "ACLineSegment", "AC Line Segments", allACLines);
+	    this.createElements(cimNetwork, "ConnectivityNode", "Connectivity Nodes", allConnectivityNodes);
+
+	    d3.selectAll("li.ACLineSegment")
+		.on("mouseover.hover", this.hover)
+		.on("mouseout", this.mouseOut);
+	},
+
+	createElements: function(cimNetwork, name, printName, data) {
+	    let elementsTopContainer = cimNetwork
+		.append("li")
+		.attr("class", name + "s" + " list-group-item");
+	    elementsTopContainer.append("a")
+		.attr("class", "btn btn-primary btn-xs")
+		.attr("role", "button")
+		.attr("data-toggle", "collapse")
+		.attr("href", "#" + name + "sList")
+		.html(printName);
+	    elementsTopContainer.append("span")
+		.attr("class", "badge")
+		.html(data.length);
+	    let elements = elementsTopContainer
+		.append("ul")
+		.attr("id", name + "sList")
+		.attr("class", "collapse");
+	    let elementTopContainer = elements
+		.selectAll("li." + name)
+		.data(data)
+		.enter()
+		.append("li")
+		.attr("class", name)
+	    elementTopContainer.append("a")
+		.attr("class", "btn btn-primary btn-xs")
+		.attr("role", "button")
+		.attr("data-toggle", "collapse")
+		.attr("href", function(d) {return "#" + d.attributes[0].value;})
+		.html(function (d) {return d.getAttribute("cim:IdentifiedObject.name").textContent});
+	    let elementEnter = elementTopContainer
+		.append("ul")
+		.attr("id", function(d) {return d.attributes[0].value;})
+		.attr("class", "collapse");
+	    elementEnter
+		.selectAll("li")
+		.data(function(d) {return d.getAttributes();})
+		.enter()
+		.append("li")
+		.html(function (d) {return d.localName.split(".")[1] + ": " + d.innerHTML});
+	},
+
+	hover: function(hoverD) {
+	    /*d3.selectAll("li.ACLineSegment")
+	      .filter(function (d) {return d == hoverD;})
+	      .style("background", "#94B8FF");*/
+	    d3.selectAll("g.ACLineSegment")
+		.filter(function (d) {return d == hoverD;})
+		.style("stroke", "black")
+		.style("stroke-width", "2px");
+	},
+
+	mouseOut: function() {
+	    d3.selectAll("li.ACLineSegment").style("background", "white");
+	    d3.selectAll("g.ACLineSegment").style("stroke-width", "0px");
 	}
-	let ioEdges = this.model.getGraph(allDiagramObjects, "DiagramObject.IdentifiedObject");
-	if (diagram.length > 0) {
-	    let graphicObjects = ioEdges.map(el => el.source);
-	    allACLines = allACLines.filter(acl => graphicObjects.indexOf(acl) > -1);
-	}
-	let nodes = allACLines;
-	
-	// navtree
-	d3.select("#app-spreadsheet")
-	    .append("div")
-	    .attr("class", "tree");
-	
-	let cimNetwork = d3.select("div.tree")
-	    .append("ul")
-	    .attr("class", "CIMNetwork list-group");
-	this.createElements(cimNetwork, "ACLineSegment", "AC Line Segments", allACLines);
-	this.createElements(cimNetwork, "ConnectivityNode", "Connectivity Nodes", allConnectivityNodes);
-	
-	d3.selectAll("li.ACLineSegment")
-	    .on("mouseover.hover", this.hover)
-	    .on("mouseout", this.mouseOut);
-    },
-
-    createElements: function(cimNetwork, name, printName, data) {
-	let elementsTopContainer = cimNetwork
-	    .append("li")
-	    .attr("class", name + "s" + " list-group-item");
-	elementsTopContainer.append("a")
-	    .attr("class", "btn btn-primary btn-xs")
-	    .attr("role", "button")
-	    .attr("data-toggle", "collapse")
-	    .attr("href", "#" + name + "sList")
-	    .html(printName);
-	elementsTopContainer.append("span")
-	    .attr("class", "badge")
-	    .html(data.length);
-	let elements = elementsTopContainer
-	    .append("ul")
-	    .attr("id", name + "sList")
-	    .attr("class", "collapse");
-	let elementTopContainer = elements
-	    .selectAll("li." + name)
-	    .data(data)
-	    .enter()
-	    .append("li")
-	    .attr("class", name)
-	elementTopContainer.append("a")
-	    .attr("class", "btn btn-primary btn-xs")
-	    .attr("role", "button")
-	    .attr("data-toggle", "collapse")
-	    .attr("href", function(d) {return "#" + d.attributes[0].value;})
-	    .html(function (d) {return d.getAttribute("cim:IdentifiedObject.name").textContent});
-	let elementEnter = elementTopContainer
-	    .append("ul")
-	    .attr("id", function(d) {return d.attributes[0].value;})
-	    .attr("class", "collapse");
-	elementEnter
-	    .selectAll("li")
-	    .data(function(d) {return d.getAttributes();})
-	    .enter()
-	    .append("li")
-	    .html(function (d) {return d.localName.split(".")[1] + ": " + d.innerHTML});
-    },
-
-    hover: function(hoverD) {
-	/*d3.selectAll("li.ACLineSegment")
-	    .filter(function (d) {return d == hoverD;})
-	    .style("background", "#94B8FF");*/
-	d3.selectAll("g.ACLineSegment")
-	    .filter(function (d) {return d == hoverD;})
-	    .style("stroke", "black")
-	    .style("stroke-width", "2px");
-    },
-
-    mouseOut: function() {
-	d3.selectAll("li.ACLineSegment").style("background", "white");
-	d3.selectAll("g.ACLineSegment").style("stroke-width", "0px");
-    }
-};
+    };
+}
--- js/main.js
+++ js/main.js
@@ -1,53 +1,63 @@
+"use strict";
+
 let cimFile = "";
 
-route('/diagrams/*', function(name) {
-    d3.select("#cim-diagram-controls").style("display", null);
-    document.getElementById("selectLabel").click();
-    cimTreeController.render(name);
-    cimDiagramController.render(name);
-    d3.select("svg").style("border", "1px solid black");
-});
-
+// here we choose a diagram to display
 route('/diagrams', function(name) {
+    let m = Object.assign({}, cimDiagramModel());
+    let cimDiagram = Object.assign({}, cimDiagramController());
+    let cimTree = Object.assign({}, cimTreeController());
+    
     d3.select("#cim-select-file").style("display", "none");
     d3.select("#cim-file-input-container").style("display", "none");
     d3.select("#cim-load-container").style("display", "none");
     d3.select("#cim-home-container").style("display", null);
+    d3.select("#cim-filename").html(cimFile.name);
     
-    let m = Object.create(cimDiagramModel);
     m.load(cimFile.name, function(error) {
+	d3.select("#cim-diagrams").append("option").attr("value", "#diagrams/none").text("Generate a new diagram");
 	let diagrams = m.getDiagramList();
-	for (i in diagrams) {
+	for (let i in diagrams) {
 	    d3.select("#cim-diagrams").append("option").attr("value", "#diagrams/"+diagrams[i]).text(diagrams[i]);
-	    $(".selectpicker").selectpicker("refresh");
 	}
-	cimDiagramController.setModel(m);
-	cimTreeController.setModel(m);
+	$(".selectpicker").selectpicker("refresh");
+	cimDiagram.setModel(m);
+	cimTree.setModel(m);
     });
-    $(".selectpicker").selectpicker("show");    
+    $(".selectpicker").selectpicker("show");
+
+    // here we show a certain diagram
+    route('/diagrams/*', function(name) {
+	$("#cim-diagram-controls").show();
+	$(".app-container").show();
+	$(".selectLabel").click();
+	cimDiagram.render(name);
+	cimTree.render(name);
+	cimDiagram.bindToTree();
+    });
 });
 
 // This is the initial route ("the home page").
 route(function(name) {
+    $('.selectpicker').selectpicker({container: "body"});
     d3.select("#cim-select-file").style("display", null);
     d3.select("#cim-file-input-container").style("display", null);
     $("#cim-file-input").fileinput("reset");
     $("#cim-file-input").fileinput("enable");
     $(".selectpicker").selectpicker("hide");
+    $(".app-container").hide();
     d3.select("#cim-diagram-controls").style("display", "none");
     d3.select("#cim-load-container").style("display", "none");
     d3.select("#cim-home-container").style("display", "none");
-    d3.select("svg").style("border", "0px");
-    d3.select("#cim-diagrams").select("select").selectAll("option").remove();
-    d3.select("#cim-diagrams").select("select").append("option").attr("disabled", "disabled").html("Select a diagram");
-    
-    $(".selectpicker").selectpicker();
+    d3.select("#cim-diagrams").selectAll("option").remove();
+    d3.select("#cim-diagrams").append("option").attr("disabled", "disabled").html("Select a diagram");
+    d3.select("#cim-filename").html("");    
+    $(".selectpicker").selectpicker("refresh");
     $("#cim-file-input").fileinput();
     $("#cim-file-input").on("fileloaded", function(event, file, previewId, index, reader) {
 	
 	/*d3.xml("http://ric302107:8080/com.vogella.jersey.first/rest/cim/Class/ACLineSegment")
 	.header("Accept", "application/rdf+xml")
-	.header("Origin", "")
 	.get(function(error, data) {
 	    console.log(error, data);
 	});*/
@@ -56,4 +66,6 @@ route(function(name) {
 	d3.select("#cim-load-container").style("display", null);
     });
 });
+
+// start routing
 route.start(true);
